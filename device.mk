13275cbe71c325fe44ea7bdfca12296c791cf42d
Bruno Martins <bgcngm@gmail.com>
2018-04-12 11:05:45 +0100

    griffin: Use QTI bluetooth stack
    Change-Id: Id76e6c31a6b9a203f7b2789edf724ac091afe4ab
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -129,8 +129,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0
 
 # LineageActions
 PRODUCT_PACKAGES += \
